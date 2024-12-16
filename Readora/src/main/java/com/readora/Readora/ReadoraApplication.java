e59f93a8738abb9be8a9dc559943914b417f98f8
sofiatorres25 <sofiatorres5082@gmail.com>
2024-12-16 01:31:19 -0300

    init: proyecto spring

---
+++ Readora/src/main/java/com/readora/Readora/ReadoraApplication.java
@@ -0,0 +1,13 @@
+package com.readora.Readora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadoraApplication.class, args);
+	}
+
+}
---
+++ Readora/src/test/java/com/readora/Readora/ReadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readora.Readora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
